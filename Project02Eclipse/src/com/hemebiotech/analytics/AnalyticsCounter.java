fc84d1892f330bdaab70d12f4a65d921c6c42678
U1 <U1@LAPTOP-R8QEAN9D.home>
2021-08-15 11:55:54 +0200

    Commit test

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,16 +3,9 @@ package com.hemebiotech.analytics;
 public class AnalyticsCounter {
 
 	public static void main(String args[]) throws Exception {
-		// first get input
-		// BufferedReader read = new BufferedReader (new
-		// FileReader("/Users/guillaumebrenot/Git/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt"));
-		// String count = read.readLine();
-		// System.out.println("Symptoms : " + count);
-		// count = read.readLine();
 
 		ObjAnalyser MonAnalyser = new ObjAnalyser();
 		MonAnalyser.listOfSymptoms();
-
 
 		System.out.println("TEST symptomName : " + MonAnalyser.symptomName);
 		System.out.println("TEST ReadSymptoms : " + MonAnalyser.ReadSymptoms);
@@ -20,15 +13,3 @@ public class AnalyticsCounter {
 		System.out.println("TEST MonAnalyser : " + MonAnalyser);
 	}
 }
-
-/*
- * BufferedReader reader = new BufferedReader (new FileReader(
- * "/Users/guillaumebrenot/Git/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt"
- * )); String line = reader.readLine();
- * 
- * for (int i = 0;line!=null;i++) { line = reader.readLine();
- * System.out.println("ligne : " + i); System.out.println("Symptoms : " + line);
- * System.out.println();
- * 
- * if (line==null) { reader.close(); } }
- */
