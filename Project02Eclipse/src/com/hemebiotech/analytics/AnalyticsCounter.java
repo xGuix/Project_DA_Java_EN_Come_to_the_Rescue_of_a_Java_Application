25ad39246ed18ee299f9b38bb0fa83bfe3423b4f
xGuix <guillaumebrenot@gmail.com>
2021-08-24 22:32:50 +0200

    Commentaire code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,25 +1,21 @@
 package com.hemebiotech.analytics;
-
-import java.io.IOException;
-
 public class AnalyticsCounter
 {
-	public static void main(String args[]) throws IOException
+	public static void main(String args[]) throws Exception
 	{
 		// Je déclare mes fichier d'entrées et de sorties
 		final String inputFile = "symptoms.txt";
 		final String outputFile = "result.out";
-		
 		// J'instancie ma class Objet d'analyse "ObjAnalyser"
 		ObjAnalyser MonAnalyser = new ObjAnalyser(new ReadSymptomDataFromFile(inputFile),
 		new CountSymptomDataFromFile(outputFile), new WriteSymptomDataToFile(outputFile));
-			// J'appelle ma methode pour obtenir la liste des symptoms du fichier "symptoms.txt"
+			// J'appelle ma méthode pour obtenir la liste des symptoms du fichier "symptoms.txt"
 			MonAnalyser.getSymptomsList();
 		    System.out.println(MonAnalyser.listSymptoms);
-		    
-		    MonAnalyser.getSymptomsCounted();
+		    // J'appelle ma méthodes pour compter les iterations de Sting
+		    MonAnalyser.getCountedList();
 		    System.out.println(MonAnalyser.listSymptomsCounted);
-		    
+		    // J'appelle ma méthode pour écrire le résultat dans le fichier "result.out" 
 		    // MonAnalyser.getSymptomsWrited();		   
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ObjAnalyser.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ObjAnalyser.java
@@ -6,54 +6,57 @@ import java.util.TreeMap;
 public class ObjAnalyser
 {
 
-	//J'ai besoin d'une façon de lire
+	// J'ai besoin d'une façon de lire
 	private ISymptomReader symptomsReader;
-	//J'ai besoin d'une façon d'écrire le résultat
+	// J'ai besoin d'une façon d'écrire le résultat
 	private ISymptomWriter symptomsWriter;
-	//J'ai besoin d'une façon de compter et trier
+	// J'ai besoin d'une façon de compter et trier
 	private ISymptomCounter symptomsCounter;
-
+	// J'ai besoin de 2 listes pour stocker mes données
 	List<String> listSymptoms;
 	TreeMap<String,Integer> listSymptomsCounted;
 	
-	//Mon constructeur me permet de construire un objet 
-	//ObjAnalyser avec les caractéristiques que je lui donne
+	// Mon constructeur me permet de construire un objet ObjAnalyser
+	// Avec les caractéristiques de mes 3 interfaces Reader, Counter, Writer
 	public ObjAnalyser(ISymptomReader symptomsReader, ISymptomCounter symptomsCounter, ISymptomWriter symptomsWriter)
 	{
 		this.symptomsReader = symptomsReader;
 		this.symptomsCounter = symptomsCounter;
 		this.symptomsWriter = symptomsWriter;
 	}
-	
+	// Je déclare ma méthodes pour Lire via l'interface ISymptomsReader
+	// Renvoi le résultat SymptomsReader
 	public ISymptomReader symptomsReader()
 	{
 		this.symptomsReader = symptomsReader();
 		return symptomsReader;
 	}
-	
+	// Je déclare ma méthodes pour Compter via l'interface ISymptomsCounter
+	// Renvoi le résultat symptomsCounter
 	public ISymptomCounter symptomsCounter()
 	{
 		this.symptomsCounter = symptomsCounter();
 		return symptomsCounter;
 	}
-	
+	// Je déclare ma méthodes pour Ecrire via l'interface ISymptomsWriter
+	// Renvoi le résultat symptomsWriter
 	public ISymptomWriter symptomsWriter()
 	{
 		this.symptomsWriter = symptomsWriter();
 		return symptomsWriter;
 	}
-
+	// Je déclare ma méthode récuperer la liste symptoms via l'interface
 	public void getSymptomsList()
 	{
 		this.listSymptoms = symptomsReader.GetSymptoms();
 	}
-	
-	public void getSymptomsCounted()
+	// Je déclare ma méthode récuperer la liste symptoms compté via l'interface
+	public void getCountedList()
 	{
 		this.listSymptomsCounted = symptomsCounter.getSymptomsCounted(listSymptoms);
 	}
-		
-	public void getSymptomsWrited() throws Exception
+	// Je déclare ma méthode ecrire la liste compté
+	public void getWritedList() throws Exception
 	{
 		symptomsWriter.getSymptomsWrited(listSymptomsCounted);
 	}
