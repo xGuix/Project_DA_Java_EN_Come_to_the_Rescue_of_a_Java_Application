30f77bc8e12930f9f9bcc053434642e6e4e58b1a
xGuix <guillaumebrenot@gmail.com>
2021-08-24 14:52:20 +0200

    Branch Objet_&_interfaces > Fonction_Main, merging done!

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,16 +1,19 @@
 package com.hemebiotech.analytics;
+
 import java.io.IOException;
 
 public class AnalyticsCounter
 {
 	public static void main(String args[]) throws IOException
 	{
+		// Je déclare mes fichier d'entrées et de sorties
 		final String inputFile = "symptoms.txt";
 		final String outputFile = "result.out";
 		
+		// J'instancie ma class Objet d'analyse "ObjAnalyser"
 		ObjAnalyser MonAnalyser = new ObjAnalyser(new ReadSymptomDataFromFile(inputFile),
 		new CountSymptomDataFromFile(outputFile), new WriteSymptomDataToFile(outputFile));
-
+			// J'appelle ma methode pour obtenir la liste des symptoms du fichier "symptoms.txt"
 			MonAnalyser.getSymptomsList();
 		    System.out.println(MonAnalyser.listSymptoms);
 		    
