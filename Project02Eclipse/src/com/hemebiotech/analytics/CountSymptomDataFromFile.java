8d263acd2a98c874f02ca9845e434666f34dfc8f
xGuix <guillaumebrenot@gmail.com>
2021-08-26 22:22:02 +0200

    version sans comptage String

--- Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/CountSymptomDataFromFile.java
@@ -4,39 +4,42 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
-
-import javax.sound.sampled.Line;
 
 public class CountSymptomDataFromFile implements ISymptomCounter
 {
 	private String filepath;
+	// Je créer ma methode pour lire la liste en mémoire symptomList
 	public CountSymptomDataFromFile(String filepath)
 	{
 		this.filepath = filepath;
 	}
-	
+	// Je creer ma methode TreeMap pour recuperer la listSymptoms
 	public TreeMap<String, Integer> getSymptomsCounted(List<String> listSymptoms) throws Exception
 	{
+		// J'instancie ma TreeMap counter pour récuperer le résultat du triage et du comptage
 		TreeMap<String, Integer> counter = new TreeMap<String,Integer>();
-
+		// Je créer me conditions d'analyse et de de comptage
 		if (filepath != null)
 		{
 			try {
+				// J'instancie mon Reader pour lire les lignes de ma listSymptom
 				BufferedReader reader = new BufferedReader(new FileReader(filepath));
 				String count = reader.readLine();
 				Integer symptomNumber = 0;
+				// Je crée une boucle de lecture listSymptomms pour les récuperer
 				for (String symptom : listSymptoms)
-				{
+				{	
 					count=symptom;
-					// System.out.println("Symptoms : "+ symptom);
-					if (count.equals(symptom))
-					{
-					symptomNumber++;
+					count.compareTo(symptom);
+					// J'ajoute chaque ligne à ma TreeMap 
 					counter.put(symptom,symptomNumber);
-					symptomNumber = Integer.valueOf(symptomNumber);
-					// System.out.println(count);
+
+					// Je compare ma nouvelle entrée à la précedente
+					if (symptom.equals(count))
+					{
+						symptomNumber++;
+						count.compareTo(symptom);
 					}
 				}
 				reader.close();
