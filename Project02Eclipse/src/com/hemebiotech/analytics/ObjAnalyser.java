2d94a35dcedb24c2e2f0681ae28c5a8afb7eb6e7
xGuix <guillaumebrenot@gmail.com>
2021-08-29 22:47:23 +0200

    ok MaJ

--- Project02Eclipse/src/com/hemebiotech/analytics/ObjAnalyser.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ObjAnalyser.java
@@ -5,58 +5,58 @@ import java.util.TreeMap;
 
 public class ObjAnalyser
 {
-	// J'ai besoin d'une façon de lire
+	// J'ai besoin d'une faï¿½on de lire
 	private ISymptomReader symptomsReader;
-	// J'ai besoin d'une façon d'écrire le résultat
+	// J'ai besoin d'une faï¿½on d'ï¿½crire le rï¿½sultat
 	private ISymptomWriter symptomsWriter;
-	// J'ai besoin d'une façon de compter et trier
+	// J'ai besoin d'une faï¿½on de compter et trier
 	private ISymptomCounter symptomsCounter;
-	// J'ai besoin de 2 listes pour stocker mes données
+	// J'ai besoin de 2 listes pour stocker mes donnï¿½es
 	List<String> listSymptoms;
 	TreeMap<String,Integer> listSymptomsCounted;
 	
 	// Mon constructeur me permet de construire un objet ObjAnalyser
-	// Avec les caractéristiques de mes 3 interfaces Reader, Counter, Writer
+	// Avec les caractï¿½ristiques de mes 3 interfaces Reader, Counter, Writer
 	public ObjAnalyser(ISymptomReader symptomsReader, ISymptomCounter symptomsCounter, ISymptomWriter symptomsWriter)
 	{
 		this.symptomsReader = symptomsReader;
 		this.symptomsCounter = symptomsCounter;
 		this.symptomsWriter = symptomsWriter;
 	}
-	// Je déclare ma méthodes pour Lire via l'interface ISymptomsReader
-	// Renvoi le résultat SymptomsReader
+	// Je dï¿½clare ma mï¿½thodes pour Lire via l'interface ISymptomsReader
+	// Renvoi le rï¿½sultat SymptomsReader
 	public ISymptomReader symptomsReader()
 	{
 		this.symptomsReader = symptomsReader();
 		return symptomsReader;
 	}
-	// Je déclare ma méthodes pour Compter via l'interface ISymptomsCounter
-	// Renvoi le résultat symptomsCounter
+	// Je dï¿½clare ma mï¿½thodes pour Compter via l'interface ISymptomsCounter
+	// Renvoi le rï¿½sultat symptomsCounter
 	public ISymptomCounter symptomsCounter()
 	{
 		this.symptomsCounter = symptomsCounter();
 		return symptomsCounter;
 	}
-	// Je déclare ma méthodes pour Ecrire via l'interface ISymptomsWriter
-	// Renvoi le résultat symptomsWriter
+	// Je dï¿½clare ma mï¿½thodes pour Ecrire via l'interface ISymptomsWriter
+	// Renvoi le rï¿½sultat symptomsWriter
 	public ISymptomWriter symptomsWriter()
 	{
 		this.symptomsWriter = symptomsWriter();
 		return symptomsWriter;
 	}
-	// Je déclare ma méthode récuperer la liste symptom via l'interface
+	// Je dï¿½clare ma mï¿½thode rï¿½cuperer la liste symptom via l'interface
 	public void getSymptomsList()
 	{
 		this.listSymptoms = symptomsReader.GetSymptoms();
 	}
-	// Je déclare ma méthode compter et trier ma liste symptom via TreeMAp
+	// Je dï¿½clare ma mï¿½thode compter et trier ma liste symptom via TreeMAp
 	public void getCountedList() throws Exception
 	{
 		this.listSymptomsCounted = symptomsCounter.getSymptomsCounted(listSymptoms);
 	}
-	// Je déclare ma méthode ecrire la liste compté via TreeMap
+	// Je dï¿½clare ma mï¿½thode ecrire la liste comptï¿½ via TreeMap
 	public void getWritedList() throws Exception
 	{
-		symptomsWriter.getSymptomsWrited(listSymptomsCounted);
+		this.symptomsWriter.getSymptomsWrited(listSymptomsCounted);
 	}
 }
